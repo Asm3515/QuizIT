219c9b4f48f4ac7de164f804be3063d4967e194f
Asm3515 <ajinkya.more3515@gmail.com>
2024-07-20 19:49:57 -0400

    Ran Into Issue rolling back

--- Backend/Routes/PlaylistRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const PlaylistController = require("../Controller/PlaylistController")
-
-router.post('/', PlaylistController.createPlaylist);
-router.delete('/:username/:playlistNumber', PlaylistController.deletePlaylist);
-router.get('/:username', PlaylistController.getPlaylistsByUser);
-
-module.exports = router;
--- Backend/Routes/SongRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const SongController = require('../Controller/SongController');
-
-router.post('/add', SongController.addSongsToPlaylist);
-router.post('/delete', SongController.deleteSongsFromPlaylist);
-
-module.exports = router;
